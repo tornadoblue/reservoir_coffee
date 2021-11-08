26bbf0cc65ec540651389986d9c55d164f98034c
Jordan <jordanabel2359@gmail.com>
2021-11-08 01:24:22 -0800

    added basename to router

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     }
 
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={"/reservoir_coffee"}>
             <ThemeProvider>
                 <Sidebar isOpen={isOpen} toggle={toggle}/>
                 <Navbar toggle={toggle}/>
