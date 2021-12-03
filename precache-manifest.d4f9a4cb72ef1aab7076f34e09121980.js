c0938745becf49237b6d46f192b108ca5d553713
jordan <jordanabel43@gmail.com>
2021-12-02 20:43:35 -0800

    Updates

---
+++ precache-manifest.d4f9a4cb72ef1aab7076f34e09121980.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf60aa6febd762e94ac95aaf8b65bca",
+    "url": "/reservoir_coffee/index.html"
+  },
+  {
+    "revision": "8eef62dd8f1e0f2a2d6d",
+    "url": "/reservoir_coffee/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "94d620a467df13219612",
+    "url": "/reservoir_coffee/static/js/2.5bb5fd42.chunk.js"
+  },
+  {
+    "revision": "97f1161acd0e6d97385ef7b190478233",
+    "url": "/reservoir_coffee/static/js/2.5bb5fd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ca6b3ae49b0000e234",
+    "url": "/reservoir_coffee/static/js/3.2ccada5f.chunk.js"
+  },
+  {
+    "revision": "8eef62dd8f1e0f2a2d6d",
+    "url": "/reservoir_coffee/static/js/main.74cb1dba.chunk.js"
+  },
+  {
+    "revision": "404760191252fcf42163",
+    "url": "/reservoir_coffee/static/js/runtime-main.52705e27.js"
+  }
+]);
